656a019a8165db3486ff2ab55e922eafa3644137
Clark Wong <clarkwkw@yahoo.com.hk>
2021-07-24 15:17:08 +0800

    add dummy screens

--- frontend/src/app/routes.js
+++ frontend/src/app/routes.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense } from 'react';
 import { withRouter } from "react-router";
 import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from "prop-types";
@@ -9,6 +9,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import LoadingBackdrop from './components/shared/loadingBackdrop';
 import HomeScreen from './components/homeScreen';
 import AboutScreen from './components/aboutScreen';
+import DummyScreen1 from './components/dummyScreen1';
+import DummyScreen2 from './components/dummyScreen2';
+import DummyScreen3 from './components/dummyScreen3';
+import DummyScreen4 from './components/dummyScreen4';
+import DummyScreen5 from './components/dummyScreen5';
+import DummyScreen6 from './components/dummyScreen6';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -28,6 +34,12 @@ const RoutesProvider = () => {
                 <Switch>
                     <Route exact path='/' component={HomeScreen}/>
                     <Route exact path='/about' component={AboutScreen}/>
+                    <Route exact path='/dummy1' component={DummyScreen1}/>
+                    <Route exact path='/dummy2' component={DummyScreen2}/>
+                    <Route exact path='/dummy3' component={DummyScreen3}/>
+                    <Route exact path='/dummy4' component={DummyScreen4}/>
+                    <Route exact path='/dummy5' component={DummyScreen5}/>
+                    <Route exact path='/dummy6' component={DummyScreen6}/>
                     <Route component={RedirectHome} />
                 </Switch>
             </Suspense>
